90fe3c3140158cdaf9a7d3b9bc24e4af59e5f92b
Jiaying Zheng <122384396+jiaying021218@users.noreply.github.com>
2024-04-12 10:55:52 -0400

    Delete townService/supabaseClient.ts

--- townService/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://slqtkecsqggjjtdathmo.supabase.co';
-const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNscXRrZWNzcWdnamp0ZGF0aG1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExMjkzOTEsImV4cCI6MjAyNjcwNTM5MX0._3xkjSurb6wIXYxREBRMEUQHvniiMUgtWaJHwXuj2-8';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
